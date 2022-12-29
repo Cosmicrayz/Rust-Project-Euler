a202cb467b044b3dfe6fb1fd9ebc39b70db73cf3
cosmic1998@gmail.com <cosmic1998@gmail.com>
2022-12-29 21:58:19 +0000

    Completed problem 1

---
+++ question_1/src/main.rs
@@ -0,0 +1,13 @@
+/*
+Prompt: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Link: https://projecteuler.net/problem=1
+ */
+
+fn main() {
+    let sum: usize = (1..1000)
+        .filter(|&num| (num % 3 == 0) || (num % 5 == 0))
+        .sum();
+    println!("The sum is {}", sum);
+}
