9b37b7b4b61305174d5a8f778679ac7bf6ef428b
cosmic1998@gmail.com <cosmic1998@gmail.com>
2023-01-02 18:36:30 +0000

    Updated file names

--- question_4/target/debug/deps/question_4.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_4\target\debug\deps\question_4.exe: src\main.rs
-
-c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_4\target\debug\deps\question_4.d: src\main.rs
-
-src\main.rs:
--- question_4/target/debug/question_4.d
+++
@@ -1,1 +0,0 @@
-c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_4\target\debug\question_4.exe: c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_4\src\main.rs
