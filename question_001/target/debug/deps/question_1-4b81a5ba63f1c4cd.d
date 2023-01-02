1f84d4da747aefd5efdb48e1915490d3634fcdf7
cosmic1998@gmail.com <cosmic1998@gmail.com>
2023-01-02 20:43:31 +0000

    Cleaned up folders

---
+++ question_001/target/debug/deps/question_1-4b81a5ba63f1c4cd.d
@@ -0,0 +1,5 @@
+c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_1\target\debug\deps\question_1-4b81a5ba63f1c4cd.rmeta: src\main.rs
+
+c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_1\target\debug\deps\question_1-4b81a5ba63f1c4cd.d: src\main.rs
+
+src\main.rs:
