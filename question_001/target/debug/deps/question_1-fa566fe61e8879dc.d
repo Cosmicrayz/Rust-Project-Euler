a901cd0dba1f202f8aeb7cfb61614b2a4d243209
cosmic1998@gmail.com <cosmic1998@gmail.com>
2023-01-02 20:47:39 +0000

    Delete target file

--- question_001/target/debug/deps/question_1-fa566fe61e8879dc.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_1\target\debug\deps\question_1-fa566fe61e8879dc.rmeta: src\main.rs
-
-c:\Users\Cosmic\Documents\Rust\Rust-Project-Euler\question_1\target\debug\deps\question_1-fa566fe61e8879dc.d: src\main.rs
-
-src\main.rs:
